0fd8f9d2e90b3fef58abb5557af061a3a8eeea4a
Yakir Bukris <yakirbu@Yakirs-MacBook-Pro.local>
2022-12-16 18:58:17 +0200

    updates

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -3,26 +3,36 @@ using System.Collections;
 
 // For usage apply the script directly to the element you wish to apply parallaxing
 // Based on Brackeys 2D parallaxing script http://brackeys.com/
-public class Parallax : MonoBehaviour {
-    private float length, startPos;
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
     public GameObject cam;
     public float parallaxEffect;
-    public SpriteRenderer floor;
-    // private float _boundMargin = 10f;
+    private PlayerController _playerController;
+    private float camPosOffset;
     
-    void Start () {
-        startPos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
-    
-    void Update ()
+
+    void Start()
     {
-        float dist = (cam.transform.position.x * parallaxEffect);
-        float newX = dist + startPos + length;
-        print($"new_x = {newX} ({gameObject.name})\n length = {length} ({gameObject.name})\n max_x = {floor.bounds.max.x}");
-        if (newX < floor.bounds.max.x)
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        _playerController = FindObjectOfType<PlayerController>();
+        camPosOffset = cam.transform.position.x;
+    }
+
+    private bool first = true;
+    void Update()
+    {
+        if(Mathf.Abs(_playerController.transform.position.x - transform.position.x) > 15)
         {
-            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+            return;
         }
+        if (first)
+        {
+            camPosOffset = cam.transform.position.x;
+            first = false;
+        }
+        float dist = ((cam.transform.position.x - camPosOffset) * parallaxEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
     }
 }
